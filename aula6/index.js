f423a66e7cedd7743fc8648ff9cfb423fdaea357
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-25 20:40:02 -0300

    25022020 - Exemplos JS.

---
+++ aula6/index.js
@@ -0,0 +1,7 @@
+let nome = 'Vinicius';
+
+console.log(nome, 'nasceu em 1988');
+console.log('Em 2016', nome, 'conheceu Fernanda.');
+console.log(nome, 'casou-se com Fernanda em 2012');
+console.log('Fernanda teve 1 filho com', nome ,'em 2018');
+console.log('O filho de', nome ,'se chama Senna.');
