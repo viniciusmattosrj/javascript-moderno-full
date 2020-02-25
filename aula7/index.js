ec14acf94f1f83c1ad417f9abce9542c070df3ab
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-25 20:57:03 -0300

    25022020 - Constantes com const.

--- aula7/index.js
+++ aula7/index.js
@@ -12,5 +12,10 @@
 */
 
 const nome = 'Vinicius';
-
 console.log(nome);
+
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+console.log(resultado);
