4b9e7b085573bfd8a3598ba8f70238a6d538da32
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-25 19:58:01 -0300

    25022020 - Exemplos de comentários.

---
+++ aula4/comentarios.js
@@ -0,0 +1,12 @@
+/* Exemplos de comentários */
+
+console.log('Olá mundo!');
+
+console.log('Linha 1');
+//console.log('Linha 2');
+/*
+    console.log('Linha 3');
+    console.log('Linha 4');
+*/
+console.log('Linha 5');
+
