8191199276009d8d5feedfa21f0ab685c7914206
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-26 00:24:51 -0300

    25022020 - Operadores Aritimeticos.

--- aula11/index.js
+++ aula11/index.js
@@ -22,3 +22,40 @@ console.log(num1 ** num2);
 
 /* % Resto da divisão */
 console.log(num1 % num2);
+
+
+/* Operadores de Atribuição */
+let contador = 2;
+contador += 2;
+console.log(contador);
+
+contador -= 2;
+console.log(contador);
+
+contador *= 3;
+console.log(contador);
+
+contador /= 4;
+console.log(contador);
+
+contador **= 5;
+console.log(contador);
+
+
+/* NaN - Not a Number */
+const num3 = 10;
+const num4 = 'Cadeira';
+console.log(num3 * num4);
+console.log(typeof num4);
+
+
+/* Conversão de números */
+const num5 = 10;
+const num6 = parseInt('5');
+console.log(num5 + num6);
+
+const num7 = parseFloat('2.5');
+console.log(num5 + num7);
+
+const num8 = Number('4.0');
+console.log(num5 + num8);
