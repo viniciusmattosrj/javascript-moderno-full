5583c3e5dd309bbbdba4a07a512ad12b4150f081
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-25 21:00:03 -0300

    25022020 - Constantes com const.

--- aula7/index.js
+++ aula7/index.js
@@ -19,3 +19,9 @@ const primeiroNumero = 5;
 const segundoNumero = 10;
 const resultado = primeiroNumero * segundoNumero;
 console.log(resultado);
+
+const resultadoDuplicado = resultado * 2;
+console.log(resultadoDuplicado);
+
+let resultadoTriplicado = resultado * 3;
+console.log(resultadoTriplicado);
