d6fd9ac244e09787eabc4ef6b951f48490ffad9d
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-26 02:00:14 -0300

    26022020 - Exercícios de solução.

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -2,8 +2,8 @@
 let num1 = prompt('Digite um número');
 let num2 = prompt('Digite um número');
 
-num1 = parseInt(num1);
-num2 = parseInt(num2);
+num1 = Number(num1);
+num2 = Number(num2);
 
 let resultado = num1 + num2;
 alert(resultado);
