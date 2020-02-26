6dcad5d985395ad0e8606f08e9755fd37257474b
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-25 22:58:02 -0300

    25022020 - Tipos Primitivos.

---
+++ aula10/index.js
@@ -0,0 +1,19 @@
+/* Entendendo os tipos de Dados Primitivos */
+
+/* string */
+const nome = 'Vinicius'; 
+const sobreNome = "Mattos";
+const ultimoNome = `Silva`;
+
+/* number */
+const idade = 32;
+const altura = 1.86;
+
+/* Undefined = não aponta para local nenhuma na memoria. */
+let nomeAluno;
+
+/* Null = não aponta para local nenhuma na memoria. */
+const sobreNomeAluno = null;
+
+/* Boolean = true false (lógico)*/
+const aprovado = true;
