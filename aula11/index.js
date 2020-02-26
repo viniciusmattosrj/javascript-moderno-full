11b81f19cce00938e7619c17d3d4a514a4cfb86a
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-25 23:56:53 -0300

    25022020 - Operadores Aritimeticos.

---
+++ aula11/index.js
@@ -0,0 +1,24 @@
+/** 
+ * Entendendo os Operadores Aritimeticos
+ */ 
+
+const num1 = 10;
+const num2 = 2;
+
+/* + Adição Concatenação */
+console.log(num1 + num2);
+
+/* - Subtração */
+console.log(num1 - num2);
+
+/* * Multiplicação */
+console.log(num1 * num2);
+
+/* / Divisão */
+console.log(num1 / num2);
+
+/* ** Potenciação */
+console.log(num1 ** num2);
+
+/* % Resto da divisão */
+console.log(num1 % num2);
