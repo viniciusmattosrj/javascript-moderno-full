c7568b20603c374855a429487b494411c1a98678
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-26 02:02:40 -0300

    26022020 - Exercícios de solução.

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -5,5 +5,4 @@ let num2 = prompt('Digite um número');
 num1 = Number(num1);
 num2 = Number(num2);
 
-const resultado = num1 + num2;
-alert(`O resultado da sua conta foi: ${resultado}`);
+alert(`O resultado da sua conta foi: ${num1 + num2}`);
