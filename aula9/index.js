003417d15f793a44aa590d7d5451f4352a53ebd3
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-25 22:23:46 -0300

    25022020 - Diferenças entre Const e Let.

---
+++ aula9/index.js
@@ -0,0 +1,9 @@
+/* ECMAScript - È a padronização do Javascript criado pela empresa ECMA Internation 
+
+    1. O var é de acesso global (má prática)
+    2. Não informar var, let ou const gera uma váriavel global. Idem item 1
+    3. O let não permite ser redeclarado a variavel . \o/
+*/   
+
+
+
