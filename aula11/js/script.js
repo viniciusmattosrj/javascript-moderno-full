5057dc65a515b2b2e795eee6e78de690f1e4cc8a
Vinicius Mattos <vinimattos.rj@gmail.com>
2020-02-26 01:59:15 -0300

    26022020 - Exercícios de solução.

---
+++ aula11/js/script.js
@@ -0,0 +1,9 @@
+/* Entrada de dados */
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite um número');
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+let resultado = num1 + num2;
+alert(resultado);
